c4e95448c90ec7a27d3ac0d9478314c20d50c015
Skshm31 <111271949+Skshm31@users.noreply.github.com>
2022-08-19 09:28:17 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import Register from './register';
+import { React, useState } from "react";
+import List from "./searchbar";
+import "./App.css";
 import Navbar from './navbar';
-import Home from './home';
+import Home from "./home";
+import './design.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
+  const [search,setSearch]=useState("");
+  let inputHandler = (e) => {
+    //convert input text to lower case
+    var lowerCase = e.target.value.toLowerCase();
+    setSearch(lowerCase);
+  };
+
   return (
-    <Home />
+    <div className="container-fluid">
+      <div className="row">
+      <Navbar/>
+      </div>
+      <div className="container-fluid">
+      <div className="row"><label className="searchfield"> 
+        <input value={search} onChange={inputHandler} placeholder="search the blog"></input>
+       </label></div>
+      </div>
+      < Home input={search}/>
+    </div>
   );
 }
 
