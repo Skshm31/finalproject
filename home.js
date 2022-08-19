790c2fae1ebf0627159b02330de433e6b0cf73c2
Skshm31 <111271949+Skshm31@users.noreply.github.com>
2022-08-19 09:27:54 +0530

    Update home.js

--- home.js
+++ home.js
@@ -6,44 +6,45 @@ import {
 import './design.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './navbar';
-
-function Home() {
-    const [movies, setMovies] = useState([]);
-    const [search,setSearch]=useState("");
-    const searchfind=(event)=>{
-           setSearch(event.target.value);
-         }
+function Home(props) {
+    let [movies, setMovies] = useState([]);
     useEffect(() => {
-      getMovies()
-      }, [])   
+        getMovies()
+        }, [])
     const getMovies = async () => {
         const moviesData = await axios.get(`https://www.omdbapi.com/?i=tt3896198&apikey=86479a88&s=batman`)
         setMovies(moviesData.data.Search);
     }
+    console.log(movies)    
+    const filteredData = movies.filter((el) => {
+        //if no input the return the original
+        if (props.input === '') {
+            return el;
+        }
+        //return the item which contains the user input
+        else {
+            return el.Title.toLowerCase().includes(props.input)
+        }
+    })
     return (
-      <div className="container-fluid">
-        <div className="row"><Navbar/></div>
-         <div className="moviedisplay">
-          {
-            movies.map(movie => {return (<div className="col-4 carddesign"><div className="card shadow code2">
-              <div className="inner">
-                <img src={movie.Poster} alt=""className="card-img-top"/>
+        <div className="container-fluid">
+            <div className="moviedisplay">
+            {
+              filteredData.map(movie => {return (<div className="col-4 carddesign"><div className="card shadow code2">
+                <div className="inner">
+                  <img src={movie.Poster} alt=""className="card-img-top"/>
+                  </div>
+                <div className="card-body text-center design">
+                  <h3 className="card-title">{movie.Title}</h3>
+                  <p className="card-text">
+                    
+                  </p>
+                  <div className="btn btn-primary"><span className="buttonsize">More Details...</span></div>
                 </div>
-              <div className="card-body text-center design">
-                <h3 className="card-title">{movie.Title}</h3>
-                <p className="card-text">
-                  
-                </p>
-                <div className="btn btn-primary"><span className="buttonsize">More Details...</span></div>
-              </div>
-          </div></div>)})
-          }</div>
-        </div>
-        
-        
-    )
+            </div></div>)})
+            }</div>
+            </div>
+      )
 }
-export default Home;
 
-
-                
+export default Home
